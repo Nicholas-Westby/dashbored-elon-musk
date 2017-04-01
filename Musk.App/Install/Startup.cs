a60fb055555edea5288285b56bc16a45431e971e
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2017-04-01 00:02:06 -0700

    DashBoard Elon Musk implementation.

---
+++ Musk.App/Install/Startup.cs
@@ -0,0 +1,66 @@
+﻿namespace Musk.App.Install
+{
+
+    // Namespaces.
+    using Properties;
+    using System.Linq;
+    using System.Web.Configuration;
+    using System.Xml;
+    using umbraco.cms.businesslogic.packager;
+    using Umbraco.Core;
+    using Umbraco.Core.Configuration.Dashboard;
+
+    /// <summary>
+    /// Handles startup events.
+    /// </summary>
+    public class Startup : ApplicationEventHandler
+    {
+
+        #region Event Handlers
+
+        /// <summary>
+        /// Application started event handler.
+        /// </summary>
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication,
+            ApplicationContext applicationContext)
+        {
+            EnsureDashboardConfigExists();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Ensures the "Musk" section exists in the dashboard.config.
+        /// </summary>
+        private void EnsureDashboardConfigExists()
+        {
+            var exists = DashboardExists();
+            if (!exists)
+            {
+                var doc = new XmlDocument();
+                var actionXml = Resources.TransformDashboard;
+                doc.LoadXml(actionXml);
+                PackageAction.RunPackageAction("Musk", "Musk.TransformXmlFile", doc.FirstChild);
+            }
+        }
+
+        /// <summary>
+        /// Indicates whether or not the "MuskSection" exists in the dashboard.config.
+        /// </summary>
+        private bool DashboardExists()
+        {
+            var config = WebConfigurationManager.OpenWebConfiguration("~");
+            var dashboard = config.GetSection("umbracoConfiguration/dashBoard")
+                as IDashboardSection;
+            var hasMusk = dashboard.Sections.Any(x =>
+                "MuskSection".InvariantEquals(x.Alias));
+            return hasMusk;
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+An Umbraco dashboard, that shows tweets from bored Elon Musk:
+
+![DashBored Elon Musk](assets/images/dash-bored.png?raw=true "DashBored Elon Musk")
